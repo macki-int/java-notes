7d1fbc31e7beee50e7ad2887dc4b3e913868c321
macki-int <macki@int.pl>
2021-09-20 00:03:55 +0200

    add method loopFor

--- src/main/java/com/mj/notes/Main.java
+++ src/main/java/com/mj/notes/Main.java
@@ -1,4 +1,16 @@
 package com.mj.notes;
 
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(loopFor(4));
+    }
+
+    static int loopFor(int maxValue) {
+        int counter = 0;
+        for (int i = 0; i < maxValue; i++) {
+            counter++;
+        }
+        return counter;
+    }
 }
